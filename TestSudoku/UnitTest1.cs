ae43a4c2dedbb914f715c1ce6ac01dd96dd85d3c
unknown <yochayk@YOCHAYWIN8_2.redmond.corp.microsoft.com>
2013-06-25 17:30:33 -0700

    Test project

---
+++ TestSudoku/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestSudoku
+{
+    [TestClass]
+    public class SudokuSolverTest
+    {
+        [TestMethod]
+        public void TestWhichFails()
+        {
+            Assert.Fail("Sudoku solver failed to solve ");
+        }
+
+        [TestMethod]
+        public void TestWhichSuccess()
+        {
+            
+        } 
+    }
+}
